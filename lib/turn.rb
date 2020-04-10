49fafe050ecffa48299041b407355a3f7bf2b28a
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-10 14:18:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board,index)
-    return board[index] = "X"
+    return display_board(board,index)
   else
     turn(board)
   end
