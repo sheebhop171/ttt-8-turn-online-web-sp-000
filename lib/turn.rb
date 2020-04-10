ba801e095800a22275e0178ff675f5264a7510c5
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-10 14:24:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,34 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " #row1
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " #row2
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " #row3
+  puts
+end
+
+def input_to_index(user_input)
+  return user_input = user_input.to_i - 1
+end
+
+def valid_move?(board,index)
+ if index.between?(0,8) && position_taken?(board,index) != true
+   true
+ else
+ end
+end
+
+def position_taken?(board,index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+  end
+end
+
+def move(array,index,value='X')
+ return array[index] = value
+end
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
