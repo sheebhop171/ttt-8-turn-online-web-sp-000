469e2b88aaac1df85c8829801b279f8d17b95e31
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-10 13:28:21 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " #row1
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " #row2
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " #row3
+  puts
+end
